7d8a10352f8bf04bb69aa2a77cd699a570142e4c
ThiagoKSR <thiagoklauber@gmail.com>
2025-05-27 11:58:09 -0300

    Add files via upload

---
+++ Módulo-JS/src/js/front.js
@@ -0,0 +1,9 @@
+// let num1 = Number(prompt("Digite o primeiro número: "));
+// let num2 = Number(prompt("Digite o segundo número: "));
+
+// let soma = num1 + num2;
+
+// alert("A soma entre os dois números é: " + soma);
+
+
+
---
+++ Módulo-JS/src/js/pratica.js
@@ -0,0 +1,16 @@
+// let nome = "Thiago,";
+// let cidade = "Jaboatão Dos Guararapes,";
+// let idade = 32;
+// let estuda = true;
+// let contato = 81000000000;
+
+// console.log(nome, cidade, idade, estuda, contato);
+
+num1 = 23;
+num2 = 10;
+
+console.log(`A soma entre entre ${num1} e ${num2} é: ${num1 + num2}`);
+console.log(`A subtração entre ${num1} e ${num2} é: ${num1 - num2}`);
+console.log(`A multiplicação entre ${num1} e ${num2} é: ${num1 * num2}`);
+console.log(`A Divisão entre ${num1} e ${num2} é: ${num1 / num2}`);
+console.log(`A Exponenciação entre ${num1} e ${num2} é: ${num1 ** num2}`);
