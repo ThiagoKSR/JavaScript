468a8ad34f02e60c8fa47a2c9cfb90a1eedc16a8
ThiagoKSR <thiagoklauber@gmail.com>
2025-06-03 11:55:44 -0300

    Add files via upload

---
+++ aula02JS.js
@@ -0,0 +1,71 @@
+// let numero1 = Number(prompt("Digite um valor: "));
+// let numero2 = Number(prompt("Digite outro valor: "));
+
+// const ambosPositivos = numero1 > 0 && numero2 > 0 ;
+
+// const umPositivo = numero1 > 0 || numero2 > 0 ;
+
+// const ambosNegativos = numero1 < 0 && numero2 < 0 ;
+
+// console.log("Ambos positivos", ambosPositivos)
+// console.log("Ambos negativos", ambosNegativos)
+// console.log("Um positivo", umPositivo)
+
+// let num1 = Number(prompt("Digite um valor: "));
+// let num2 = Number(prompt("Digite outro valor: "));
+
+// const maiorQue = num1 > num2;
+// const igual = num1 == num2;
+// const estritIgual = num1 === num2;
+// const diferente = num1 != num2;
+// const estritDife = num1 !== num2;
+
+// console.log("Maior que", maiorQue);
+// console.log("Igual a", igual);
+// console.log("Estritamente igual", estritIgual);
+// console.log("Diferente", diferente);
+// console.log("Estritamente diferente", estritDife);
+
+// let num = Number(prompt("Digite um valor: "))
+
+// if (num > 0) {
+//   console.log("Número positivo!")
+
+// } else if (num == 0) {
+//   console.log("número neutro!")
+
+// } else console.log("Número negativo!")
+
+// let idade = 18 ;
+// let situacao = (idade >= 18) ? "Maior de idade" : "Menor de idade" ;
+
+// console.log(situacao) ;
+
+// let num1 = Number(prompt("Digite um número: "));
+// let num2 = Number(prompt("Digite outro número: "));
+// let operacao = prompt("Digite a operação: (+, -, *, /)");
+// let resultado;
+
+// switch (operacao) {
+//   case "+":
+//     resultado = num1 + num2;
+//     break;
+//   case "-":
+//     resultado = num1 - num2;
+//     break;
+//   case "*":
+//     resultado = num1 * num2;
+//     break;
+//   case "/":
+//     if (num2 === 0) {
+//       resultado = "Divisão por zero inválida.";
+//     } else {
+//       resultado = num1 / num2;
+//       break;
+//     }
+//   default:
+//     resultado = "Operação inválida";
+//     break;
+// }
+
+// alert(resultado);
