e7249fe05bec4c6e91fbfb591d295d2ba165e61d
ThiagoKSR <thiagoklauber@gmail.com>
2025-06-07 17:44:04 -0300

    Add files via upload

---
+++ aula03JS/aula03.js
@@ -0,0 +1,52 @@
+// let  contador = 0;
+
+// while (contador < 11) {
+//     console.log(contador);
+//     contador++
+// }
+
+// let numero = Number(prompt("Insira um número inicial: "));
+// let inserido;
+
+// do {
+//     inserido = Number(prompt("Insira um número: "));
+//     console.log(inserido);
+
+// } while (inserido >= numero);
+
+// let maquina = 7
+// let user;
+
+// while (true) {
+//     user = Number(prompt("Digite um valor: "));
+//     if (user === maquina) {
+//         break;
+//     }
+
+//     }
+
+// let num = Number(prompt("Digite um número inteiro positivo: "));
+// let soma = 0;
+// let contador = 0;
+
+// while (contador <= num) {
+//     if (contador % 2 === 0)
+//         soma += contador
+//     contador++
+// }
+// alert(soma)
+
+// let numero = Number(prompt("Digite um número para contar: "))
+
+// for (let i = 0; i < numero +1; i++) {
+//     if (i % 3 === 0) {
+//         continue
+    
+//     }
+//     console.log(i)
+// }
+
+// let palavra = prompt("Digite uma palavra: ")
+//  for (const letra of palavra) {
+//     console.log(letra);
+//  }
